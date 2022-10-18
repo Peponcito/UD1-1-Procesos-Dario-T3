a3dd073517dacafb46f900d6dca960e394142007
Darío Chinea Delgado <dariocd0808@gmail.com>
2022-10-18 12:54:40 +0100

    Feature: Añadí mejoras
    Se creo un nuevo txt con la iformacion del wc, y por pantalla con el readder lo pinto
    Pongo el Thread.sleep ya que al ser ascinrono se ejecuta uno nates que otro

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         ProcessBuilder processBuilder = new ProcessBuilder(
-                "C:\\Program Files\\Git\\git-bash", "-c", "cat prueba.txt | wc");
+                "C:\\Program Files\\Git\\git-bash", "-c", "cat prueba.txt | wc > resultado.txt");
         processBuilder.directory(new File("./src"));
         processBuilder.start();
+        Thread.sleep(1000);
+        File archivo = new File ("./src/resultado.txt");
+        FileReader fr = new FileReader (archivo);
+        BufferedReader br = new BufferedReader(fr);
+        System.out.println(br.readLine());
     }
 }
