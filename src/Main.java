90dd4eb6369b293eb157971c7767efb26a545e68
Darío Chinea Delgado <dariocd0808@gmail.com>
2022-10-18 12:52:50 +0100

    Feature: se añadió la funcionaliad del programa
    Dado un archivo lo lee y con el comando wc lee las línea y caracteres que tiene

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ProcessBuilder processBuilder = new ProcessBuilder(
+                "C:\\Program Files\\Git\\git-bash", "-c", "cat prueba.txt | wc");
+        processBuilder.directory(new File("./src"));
+        processBuilder.start();
     }
 }
